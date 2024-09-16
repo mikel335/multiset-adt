c32ed3114b7a4931f3f1bda9a528cf30fa384810
Olivia Zhou <oliviajq.zhou@mail.utoronto.ca>
2024-09-16 14:32:14 -0400

    added abstract class definition of MultiSet v1

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -8,7 +8,17 @@ public abstract class MultiSet {
      *
      * @param item the item to add
      */
-    abstract void add(Integer item);
 
     // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
+    abstract void add(Integer item);
+
+    abstract void remove(Integer item);
+
+    abstract void contains(Integer item);
+
+    abstract void is_empty();
+
+    abstract void count(Integer item);
+
+    abstract void size();
 }
